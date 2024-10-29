ee97b4e9350c740f67615fbc32859f9ea0c925d7
prakashebi <45974822+prakashebi@users.noreply.github.com>
2024-10-29 11:14:55 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,9 +1,23 @@
 import {
-  BedrockRuntimeClient, ConverseCommand
-} from "@aws-sdk/client-bedrock-runtime"
+  BedrockRuntimeClient,
+  ConverseCommand,
+} from "@aws-sdk/client-bedrock-runtime";
 
-const test = () => {
-  alert('chat.js')
-}
+const client = new BedrockRuntimeClient({region: "us-west-2"});
 
-export default test
+const modelId = "meta.llama3-1-8b-instruct-v1:0";
+
+const userMessage = "What is metagenomics'";
+const conversation = [
+  {
+    role: "user",
+    content: [{ text: userMessage }],
+  },
+];
+
+const response = await client.send(
+  new ConverseCommand({ modelId, messages: conversation }),
+);
+
+const responseText = response.output.message.content[0].text;
+console.log(responseText);
